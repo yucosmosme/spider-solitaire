08ebdc011c8e0362d8cc5779a93522a6ad90dbc7
ultrakeypoint <ultrakeypoint@gmail.com>
2023-03-26 23:44:46 +0900

    Arranged not used code

--- src/managers/CardManager.js
+++ src/managers/CardManager.js
@@ -1,5 +1,5 @@
 import _ from "lodash";
-import {CARD_NUMBER_MAX, CARD_SUIT_MAX} from "constants";
+import {CARD_NUMBER_MAX} from "constants";
 import {POOL} from "constants/Pool";
 import * as SUITS from "constants/Suit";
 import Card from "components/Card";
